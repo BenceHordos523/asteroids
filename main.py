be019ccef6ffc981541b25daa480cd2f91aa157b
BenceHordos523 <bence.hordos523@gmail.com>
2025-06-25 15:34:25 +0200

    container groups added

--- main.py
+++ main.py
@@ -16,11 +16,14 @@ def main():
     # init game "clock" for fps
     game_clock = pygame.time.Clock()
     dt = 0
-
+   
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     # init player to the middle of the screen
     player_x = SCREEN_WIDTH / 2
     player_y = SCREEN_HEIGHT / 2
-    player = Player(player_x, player_y)
+    player = Player(player_x, player_y) 
 
     while True:
         # quit method
@@ -31,11 +34,11 @@ def main():
         # filling the screen with black background color
         pygame.Surface.fill(screen, color="black")
         
-        # drawing player
-        player.draw(screen)
-        
-        # update the player
-        player.update(dt)
+        for item in drawable:
+            item.draw(screen)
+
+        for item in updatable:
+            item.update(dt)
 
 
         # updates the screen
