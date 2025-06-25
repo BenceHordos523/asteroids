3887ab200572e2904fd6d65b45f46fd5d5cfac5d
BenceHordos523 <bence.hordos523@gmail.com>
2025-06-25 14:44:19 +0200

    added fps

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def main():
     # initialize screen (GUI window)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
 
         for event in pygame.event.get():
@@ -20,6 +24,10 @@ def main():
 
         pygame.Surface.fill(screen, color="black")
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
+
+
 
 
 if __name__ == "__main__":
